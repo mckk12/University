e06281c8c694c8d4cfffc3ce63bb071d0c4cfd31
mckk12 <maciej12ciepiela@gmail.com>
2024-10-12 09:14:28 +0200

    finished task0 - pizzo

--- PiZZO/Task0/emulator.py
+++ PiZZO/Task0/emulator.py
@@ -1,23 +1,32 @@
 import json
-import sys
 
 # Getting the automaton from the chosen file
 def load_automat(file_path):
     with open(file_path, 'r') as f:
         return json.load(f)
 
-# Getting state change (should add dict)
-def get_state(automaton, current_state, letter):
-    for option in automaton['transitions']:
-        if option["from"] == current_state and option["letter"] == letter:
-            return option["to"]
+# Getting state change from dictionary
+def get_state(current_state, letter, trans):
+    return trans.get((current_state, letter), None)
 
+# Getting the file path from the user
 fp = input().strip()
-automaton = load_automat(fp)
+with open(fp, "r") as l:
+    lines=l
+    
+# Getting the automaton from the chosen file
+f = lines.readline().strip()
+automaton = load_automat(f)
 
+# Creating a dictionary of transitions
+trans={}
+for transition in automaton['transitions']:
+    trans[(transition["from"], transition["letter"])] = transition["to"]
+
+# Running the automaton
 current_state = automaton['initial']
 while True:
-            letter = sys.stdin.read(1)
+            letter = lines.read(1)
             if letter == '\n':
                 if current_state in automaton['accepting']:
                     print("yes")
@@ -27,6 +36,8 @@ while True:
             elif letter not in automaton["alphabet"]:
                 break
             else:
-                current_state = get_state(automaton, current_state, letter)
-        
+                current_state = get_state(current_state, letter, trans)
+                if current_state == None:
+                    break
+    
             
