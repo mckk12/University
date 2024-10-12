588debd2d4769692e511c099422b88fbcbdc7174
mckk12 <maciej12ciepiela@gmail.com>
2024-10-12 20:34:14 +0200

    .

--- PythonAdv/List01/task2.py
+++ PythonAdv/List01/task2.py
@@ -1,7 +1,8 @@
 import string
 
 def is_palindrome(word):
-    word = word.translate({ord(i): None for i in string.punctuation+' '}).lower()
+    word = word.strip(string.punctuation+' ').lower()
+    print(word.strip(string.punctuation+' ').lower())
     return word == word[::-1]
 
 print(is_palindrome("Eine güldne, gute Tugend: Lüge nie!"))
