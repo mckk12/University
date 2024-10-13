fc7b1d0c435c57f8bfe36bea8ac172dfdafb05ad
mckk12 <maciej12ciepiela@gmail.com>
2024-10-13 11:18:01 +0200

    task 5 L1 - python

--- PythonAdv/List01/task5.py
+++ PythonAdv/List01/task5.py
@@ -1,2 +1,20 @@
 # longest common prefix of min 3 elements of the list of strings
-def lcp(lst):
+def common_prefix(lst):
+    if not lst:
+        return ""
+    lst = list(map(str.lower, lst))
+    lst.sort()
+    longest_prefix = ""
+    for i in range(len(lst) - 2):
+        prefix = ""
+        for j in range(len(lst[i])):
+            if lst[i][j] == lst[i + 1][j] == lst[i + 2][j]:
+                prefix += lst[i][j]
+            else:
+                break
+        if len(prefix) > len(longest_prefix):
+            longest_prefix = prefix
+    return longest_prefix
+
+print(common_prefix(["Cyprian", "cyberotoman", "cynik", "ceniąc", "czule"]))
+
