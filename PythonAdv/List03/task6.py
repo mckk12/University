036ffaf8729b750a5abda68fcba8162f616b879f
mckk12 <maciej12ciepiela@gmail.com>
2024-10-23 23:41:04 +0200

    finished L03 - python

--- PythonAdv/List03/task6.py
+++ PythonAdv/List03/task6.py
@@ -1,36 +1,44 @@
-# lista rozwiazan, rozwiazanie = lista 4 list dlugosci 4 zawierajacych informacje o kazdym wierszy
+from itertools import product
+
+# tworzymy wszystkie mozliwosci wypelnienia kolumn -> obliczamy iloczyn kartezjanski -> filtrujemy po wartosciach na wierszach
 def cienie(v1, v2):
-    h1 = list(v1)
-    h2 = list(v2)
-    rozwiazanie = [[0 for i in range(4)] for j in range(4)]
-    for j in range(len(h1)):
-        if h1[j] > 0:
-            for i in range(len(h2)):
-                if h2[i] > 0:
-                    h1[j] -= 1
-                    while h1[j] > 0:
-                        rozwiazanie[i][j] = 1
-                        i += 1
-                        h2[i] -= 1
-                    break
-                if 
-            
+    wymiar = len(v1)
+    opcje = filter(
+        lambda x: all(
+            sum(x[i][j] for i in range(wymiar)) == v2[j] 
+            and ''.join('1'*v2[j]) in ''.join(map(str, (x[i][j] for i in range(wymiar)))) 
+            for j in range(wymiar)
+        ) ,
+        product(
+            *(
+                [
+                    [0] * j + [1] * v1[i] + [0] * (wymiar - v1[i] - j)
+                    for j in range(wymiar - v1[i] + 1)
+                ]
+                for i in range(wymiar)
+            )
+        )
+    )
+    # print(list(opcje))
+    return opcje
 
 
-    return 
 
-
-
+# i oraz j odwrotnie poniewaz chcemy po kolumnach iterowac
 def wyswietl_rozw(rozw):
-    for i in range(4):
-        for j in range(4):
+    wymiar = len(list(rozw))
+    for j in range(wymiar):
+        for i in range(wymiar):
             print("X", end=" ") if rozw[i][j] else print("O", end=" ")
         print()
     return
 
+
 zadanie = cienie((2,1,3,1), (1,3,1,2))
+
 
 for rozwiazanie in zadanie:
     wyswietl_rozw(rozwiazanie)
+    print()
 
 
