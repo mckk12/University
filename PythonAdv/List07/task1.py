558a05e4150f8f694b1e86a28ab3e4485d399edf
mckk12 <maciej12ciepiela@gmail.com>
2024-11-22 13:31:02 +0100

    pythonAdv list07

---
+++ PythonAdv/List07/task1.py
@@ -0,0 +1,26 @@
+import asyncio
+import aiohttp
+
+from prywatne import API_KEY 
+
+async def fetch_cat_facts(session):
+    async with session.get("https://cat-fact.herokuapp.com/facts") as response:
+        return await response.json()
+
+async def fetch_postman_collections(session):
+    headers = {"X-eBirdApiToken": API_KEY}
+    async with session.get("https://api.ebird.org/v2/data/obs/KZ/recent", headers=headers) as response:
+        return await response.json()
+
+async def get():
+    async with aiohttp.ClientSession() as session:
+        cat_facts_task = asyncio.create_task(fetch_cat_facts(session))
+        postman_collections_task = asyncio.create_task(fetch_postman_collections(session))
+
+        cat_facts = await cat_facts_task
+        postman_collections = await postman_collections_task
+
+        print("Cat Fact:", cat_facts[1]['text'])
+        print("Postman Collection:", postman_collections[1])
+
+asyncio.run(get())
